9485c1743c31f1202503c80b3d11cf187c177353
pakaufmann <patrick-kaufmann@gmx.ch>
2022-12-03 11:27:27 +0100

    Day 3

---
+++ src/main/kotlin/Day3.kt
@@ -0,0 +1,36 @@
+import java.io.File
+
+fun main() {
+    val rucksacks = File("inputs/day3.txt").readLines()
+    println(part1(rucksacks))
+    println(part2(rucksacks))
+}
+
+fun part1(rucksacks: List<String>): Int {
+    return rucksacks.sumOf { rucksack ->
+        val compartments = rucksack.compartments()
+        compartments.first.intersect(compartments.second).first().priority()
+    }
+}
+
+fun part2(rucksacks: List<String>): Int {
+    return rucksacks.windowed(3, 3).sumOf { group ->
+        group
+            .map { it.itemize() }.reduce { f, s -> f.intersect(s) }
+            .first()
+            .priority()
+    }
+}
+
+fun String.compartments(): Pair<Set<Char>, Set<Char>> {
+    val middle = length / 2
+    return Pair(substring(0, middle).itemize(), substring(middle).itemize())
+}
+
+fun Char.priority(): Int = if (isLowerCase()) {
+    minus('a') + 1
+} else {
+    minus('A') + 27
+}
+
+fun String.itemize(): Set<Char> = toCharArray().toSet()
